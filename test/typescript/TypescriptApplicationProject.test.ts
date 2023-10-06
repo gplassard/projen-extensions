5ce8ba03b80d1c7dacbbba1c665a967e9c5d040a
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-10-07 00:56:12 +0200

    feat(pnpm): check that we can still use yarn

--- test/typescript/TypescriptApplicationProject.test.ts
+++ test/typescript/TypescriptApplicationProject.test.ts
@@ -1,10 +1,20 @@
 import { synthSnapshot } from 'projen/lib/util/synth';
 import { TypescriptApplicationProject } from '../../src';
+import { NodePackageManager } from 'projen/lib/javascript';
 
 describe('TypescriptApplicationProject with default settings', () => {
   it('synthesizes', () => {
     const project = new TypescriptApplicationProject({
       name: 'test-project',
+    });
+    const output = synthSnapshot(project);
+    expect(output).toMatchSnapshot();
+  });
+
+  it('can still use yarn', () => {
+    const project = new TypescriptApplicationProject({
+      name: 'test-project',
+      packageManager: NodePackageManager.YARN,
     });
     const output = synthSnapshot(project);
     expect(output).toMatchSnapshot();
