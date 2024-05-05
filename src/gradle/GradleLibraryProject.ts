980302cb4b21ef100c52df78707cd908c3290071
Gabriel Plassard <gabriel.plassard@gmail.com>
2024-05-05 13:31:47 +0000

    fix lint

--- src/gradle/GradleLibraryProject.ts
+++ src/gradle/GradleLibraryProject.ts
@@ -3,8 +3,8 @@ import { GitHub } from 'projen/lib/github';
 import { GradleBuildAction, GradleBuildActionProps } from './GradleBuildAction';
 import { GradleReleaseAction, GradleReleaseActionProps } from './GradleReleaseAction';
 import { CustomGitignore, CustomGitignoreProps } from '../git';
-import { DEFAULT_PULL_REQUEST_LINT_OPTIONS } from '../github/utils';
 import { NodeJSDependenciesUpgradeAction, NodeJSDependenciesUpgradeActionProps, ProjenSynthAction, ProjenSynthActionProps } from '../github';
+import { DEFAULT_PULL_REQUEST_LINT_OPTIONS } from '../github/utils';
 
 export interface GradleLibraryProjectOptions extends ProjectOptions {
   githubLint?: boolean;
@@ -37,12 +37,13 @@ export class GradleLibraryProject extends Project {
       pullRequestLintOptions: {
         ...DEFAULT_PULL_REQUEST_LINT_OPTIONS,
         ...(options.githubLintOptions ?? {}),
-      }
-    })
-    
+      },
+    });
+
     options.gradleReleaseActionOptions && new GradleReleaseAction(this, options.gradleReleaseActionOptions);
     (options.gradleBuildAction || options.gradleBuildAction == undefined) && new GradleBuildAction(this, options.gradleBuildActionOptions ?? {});
-    (options.nodeJSDependenciesUpgradeAction || options.nodeJSDependenciesUpgradeAction == undefined) && new NodeJSDependenciesUpgradeAction(github, options.nodeJSDependenciesUpgradeActionOptions ?? {});
-    (options.projenSynthAction || options.projenSynthAction == undefined) && new ProjenSynthAction(github, options.projenSynthActionOptions ?? {})
+    (options.nodeJSDependenciesUpgradeAction || options.nodeJSDependenciesUpgradeAction == undefined)
+      && new NodeJSDependenciesUpgradeAction(github, options.nodeJSDependenciesUpgradeActionOptions ?? {});
+    (options.projenSynthAction || options.projenSynthAction == undefined) && new ProjenSynthAction(github, options.projenSynthActionOptions ?? {});
   }
 }
