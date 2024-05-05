34568f7031529afddf6fe53461139d69952741d1
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-05-05 16:35:18 +0200

    feat(rust): add support for projen and upgrade workflows in rust projects (#110)
    * feat(rust): add support for projen and upgrade workflows in rust projects
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- src/rust/RustProject.ts
+++ src/rust/RustProject.ts
@@ -4,11 +4,16 @@ import { Cargo, CargoProps } from './Cargo';
 import { RustLintAction, RustLintActionProps } from './RustLintAction';
 import { RustReleaseActions, RustReleaseActionsProps } from './RustReleaseActions';
 import { CustomGitignore, CustomGitignoreProps } from '../git';
+import { NodeJSDependenciesUpgradeAction, NodeJSDependenciesUpgradeActionProps, ProjenSynthAction, ProjenSynthActionProps } from '../github';
 import { DEFAULT_PULL_REQUEST_LINT_OPTIONS } from '../github/utils';
 
 export interface RustProjectOptions extends ProjectOptions {
   cargo: CargoProps;
   customGitignore?: CustomGitignoreProps;
+  nodeJSDependenciesUpgradeAction?: boolean;
+  nodeJSDependenciesUpgradeActionOptions?: NodeJSDependenciesUpgradeActionProps;
+  projenSynthAction?: boolean;
+  projenSynthActionOptions?: ProjenSynthActionProps;
   rustReleaseActions?: RustReleaseActionsProps;
   rustLintActions?: RustLintActionProps;
 }
@@ -33,10 +38,12 @@ export class RustProject extends Project {
     new Cargo(this, options.cargo);
     new RustReleaseActions(this, options.rustReleaseActions);
     new RustLintAction(this, options.rustLintActions);
-    new GitHub(this, {
+    const github = new GitHub(this, {
       mergify: false,
       pullRequestLintOptions: DEFAULT_PULL_REQUEST_LINT_OPTIONS,
     });
-
+    (options.nodeJSDependenciesUpgradeAction || options.nodeJSDependenciesUpgradeAction == undefined)
+    && new NodeJSDependenciesUpgradeAction(github, options.nodeJSDependenciesUpgradeActionOptions ?? {});
+    (options.projenSynthAction || options.projenSynthAction == undefined) && new ProjenSynthAction(github, options.projenSynthActionOptions ?? {});
   }
 }
