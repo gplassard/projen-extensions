10beb57828a5db5ca34a7a46b49735e49dc738b7
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-06-28 20:24:54 +0200

    feat(datadog-integrations): add datadog software composition analysis and static analysis

---
+++ src/github/DatadogSoftwareCompositionAnalysisAction.ts
@@ -0,0 +1,41 @@
+import { Component } from 'projen';
+import { GitHub, GithubCredentials, GithubWorkflow, WorkflowActions, WorkflowJobs } from 'projen/lib/github';
+import { JobPermission } from 'projen/lib/github/workflows-model';
+import { NodeJSDependenciesUpgradeActionProps } from './NodeJSDependenciesUpgradeAction';
+import { WorkflowActionsX } from './WorkflowActionsX';
+
+export interface DatadogSoftwareCompositionAnalysisActionProps {
+
+}
+
+export class DatadogSoftwareCompositionAnalysisAction extends Component {
+  constructor(scope: GitHub, _props: DatadogSoftwareCompositionAnalysisActionProps) {
+    super(scope);
+
+    const workflow = new GithubWorkflow(scope, 'Datadog Software Composition Analysis');
+    workflow.on({
+      pullRequest: {
+        branches: ['main'],
+      },
+      push: {
+        branches: ['main'],
+      },
+    });
+    workflow.addJob('software-composition-analysis', {
+      runsOn: ['ubuntu-latest'],
+      permissions: {},
+      steps: [
+        WorkflowActionsX.checkout({}),
+        {
+          name: 'Check imported libraries are secure and compliant',
+          uses: 'DataDog/datadog-sca-github-action@v2',
+          with: {
+            dd_app_key: '${{ secrets.DD_APP_KEY }}',
+            dd_api_key: '${{ secrets.DD_API_KEY }}',
+            dd_site: 'datadoghq.com',
+          },
+        },
+      ],
+    });
+  }
+}
---
+++ src/github/DatadogStaticAnalysisAction.ts
@@ -0,0 +1,43 @@
+import { Component } from 'projen';
+import { GitHub, GithubCredentials, GithubWorkflow, WorkflowActions, WorkflowJobs } from 'projen/lib/github';
+import { JobPermission } from 'projen/lib/github/workflows-model';
+import { NodeJSDependenciesUpgradeActionProps } from './NodeJSDependenciesUpgradeAction';
+import { WorkflowActionsX } from './WorkflowActionsX';
+
+export interface DatadogStaticAnalysisActionProps {
+
+}
+
+export class DatadogStaticAnalysisAction extends Component {
+  constructor(scope: GitHub, _props: DatadogStaticAnalysisActionProps) {
+    super(scope);
+
+    const workflow = new GithubWorkflow(scope, 'Datadog Static Analysis');
+    workflow.on({
+      pullRequest: {
+        branches: ['main'],
+      },
+      push: {
+        branches: ['main'],
+      },
+    });
+    workflow.addJob('static-analysis', {
+      runsOn: ['ubuntu-latest'],
+      permissions: {},
+      steps: [
+        WorkflowActionsX.checkout({}),
+        {
+          name: 'Check code meets quality and security standards',
+          uses: 'DataDog/datadog-static-analyzer-github-action@v1',
+          with: {
+            dd_app_key: '${{ secrets.DD_APP_KEY }}',
+            dd_api_key: '${{ secrets.DD_API_KEY }}',
+            dd_site: 'datadoghq.com',
+            cpu_count: 2,
+            enable_performance_statistics: true,
+          },
+        },
+      ],
+    });
+  }
+}
