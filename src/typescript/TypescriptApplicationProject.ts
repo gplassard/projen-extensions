8531492c62cb30fc0c29bff3f860d5cd8686a9df
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-08-02 21:35:54 +0200

    feat(datadog-test): remove cross env

--- src/typescript/TypescriptApplicationProject.ts
+++ src/typescript/TypescriptApplicationProject.ts
@@ -124,10 +124,9 @@ export class TypescriptApplicationProject extends TypeScriptProject {
 
     const enableDdTestInstrumentation = options.datadog?.testInstrumentation ?? true;
     if (enableDdTestInstrumentation) {
-      this.addDevDeps('cross-env');
       this.addDevDeps('dd-trace');
       this.addTask('test:ci', {
-        exec: 'cross-env NODE_OPTIONS="--import dd-trace/register.js -r dd-trace/ci/init" vitest run -u',
+        exec: 'NODE_OPTIONS="--import dd-trace/register.js -r dd-trace/ci/init ${NODE_OPTIONS:-}" vitest run -u',
       });
     } else {
       this.addTask('test:ci', {
