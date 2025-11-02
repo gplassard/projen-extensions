b1b014a83e2e2e68ef40cc8e7a1edf2295143c71
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-11-02 19:19:11 +0100

    feat(ddtrace): make ddtrace version configurable

--- src/github/utils.ts
+++ src/github/utils.ts
@@ -1,4 +1,5 @@
 import { PullRequestLintOptions } from 'projen/lib/github';
+import { version as ddTraceDefaultVersion } from './dd-trace.json';
 import * as nodeJsVersions from './nodejs.json';
 import { version as pnpmDefaultVersion } from './pnpm.json';
 
@@ -13,6 +14,8 @@ const DEFAULT_NODE_VERSION: string = NODEJS_VERSIONS.NODEJS_22_X; // LTS
 
 const DEFAULT_PNPM_VERSION: string = pnpmDefaultVersion.replace('v', '');
 
+const DEFAULT_DD_TRACE_VERSION: string = ddTraceDefaultVersion;
+
 export const DEFAULT_PULL_REQUEST_LINT_OPTIONS: PullRequestLintOptions = {
   semanticTitleOptions: {
     types: ['feat', 'fix', 'chore', 'refactor', 'build', 'docs', 'ci', 'perf', 'style', 'test'],
@@ -26,4 +29,8 @@ export function nodeVersion(options: { nodeVersion?: string; packageManager?: st
 
 export function pnpmVersion(): string {
   return DEFAULT_PNPM_VERSION;
+}
+
+export function ddTraceVersion(options: { ddTraceVersion?: string }): string {
+  return options?.ddTraceVersion ?? DEFAULT_DD_TRACE_VERSION;
 }
