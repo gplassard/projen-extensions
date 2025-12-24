7d9964f044b1eb21af79d05fa5dc82e5e2c20148
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-12-24 12:32:52 +0100

    fix(dd-actions): trigger datadog github actions on push instead of pull request (#210)
    * fix(dd-actions): trigger datadog github actions on push instead of pull request
    
    * chore: self mutation
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add static-analysis.datadog.yaml
    
    * chore: self mutation
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/github/DatadogStaticAnalysisAction.ts
+++ src/github/DatadogStaticAnalysisAction.ts
@@ -1,4 +1,4 @@
-import { Component, JsonPatch } from 'projen';
+import { Component, JsonPatch, YamlFile } from 'projen';
 import { GitHub, GithubWorkflow } from 'projen/lib/github';
 import { JobPermission } from 'projen/lib/github/workflows-model';
 import { WorkflowActionsX } from './WorkflowActionsX';
@@ -13,12 +13,7 @@ export class DatadogStaticAnalysisAction extends Component {
 
     const workflow = new GithubWorkflow(scope, 'Datadog-Static-Analysis');
     workflow.on({
-      pullRequest: {
-        branches: ['main'],
-      },
-      push: {
-        branches: ['main'],
-      },
+      push: {},
     });
     workflow.addJob('static-analysis', {
       runsOn: ['ubuntu-latest'],
@@ -29,7 +24,7 @@ export class DatadogStaticAnalysisAction extends Component {
         WorkflowActionsX.checkout({}),
         {
           name: 'Check code meets quality and security standards',
-          uses: 'DataDog/datadog-static-analyzer-github-action@v1',
+          uses: 'DataDog/datadog-static-analyzer-github-action@v2',
           with: {
             dd_app_key: '${{ secrets.DD_APP_KEY }}',
             dd_api_key: '${{ secrets.DD_API_KEY }}',
@@ -43,5 +38,64 @@ export class DatadogStaticAnalysisAction extends Component {
     workflow?.file?.patch(
       JsonPatch.add('/permissions', {}),
     );
+
+    // from https://docs.datadoghq.com/security/code_security/static_analysis/setup/?tab=github
+    new YamlFile(this, 'static-analysis.datadog.yaml', {
+      obj: {
+        'schema-version': 'v1',
+        'rulesets': [
+          'apex-code-style',
+          'apex-security',
+          'csharp-best-practices',
+          'csharp-code-style',
+          'csharp-security',
+          'csharp-inclusive',
+          'docker-best-practices',
+          'github-actions',
+          'go-best-practices',
+          'go-inclusive',
+          'go-security',
+          'java-best-practices',
+          'java-code-style',
+          'java-security',
+          'javascript-best-practices',
+          'javascript-browser-security',
+          'javascript-code-style',
+          'javascript-inclusive',
+          'javascript-common-security',
+          'javascript-express',
+          'javascript-node-security',
+          'jsx-react',
+          'kotlin-security',
+          'kotlin-inclusive',
+          'kotlin-code-style',
+          'kotlin-best-practices',
+          'php-best-practices',
+          'php-security',
+          'python-best-practices',
+          'python-code-style',
+          'python-django',
+          'python-flask',
+          'python-inclusive',
+          'python-pandas',
+          'python-security',
+          'ruby-code-style',
+          'ruby-security',
+          'ruby-best-practices',
+          'rails-best-practices',
+          'swift-code-style',
+          'swift-security',
+          'swift-inclusive',
+          'tsx-react',
+          'typescript-best-practices',
+          'typescript-browser-security',
+          'typescript-code-style',
+          'typescript-common-security',
+          'typescript-express',
+          'typescript-inclusive',
+          'typescript-node-security',
+        ],
+      },
+    });
   }
 }
