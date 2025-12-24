647416b35941edc7a867dedf9493e3b8191f4941
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2025-12-24 12:25:09 +0100

    add static-analysis.datadog.yaml

--- src/github/DatadogStaticAnalysisAction.ts
+++ src/github/DatadogStaticAnalysisAction.ts
@@ -1,4 +1,4 @@
-import { Component, JsonPatch } from 'projen';
+import { Component, JsonPatch, YamlFile } from 'projen';
 import { GitHub, GithubWorkflow } from 'projen/lib/github';
 import { JobPermission } from 'projen/lib/github/workflows-model';
 import { WorkflowActionsX } from './WorkflowActionsX';
@@ -38,5 +38,64 @@ export class DatadogStaticAnalysisAction extends Component {
     workflow?.file?.patch(
       JsonPatch.add('/permissions', {}),
     );
+
+    // from https://docs.datadoghq.com/security/code_security/static_analysis/setup/?tab=github
+    new YamlFile(this, 'static-analysis.datadog.yaml', {
+      obj: {
+        'schema-version': 'v1',
+        'rulesets': [
+          'apex-code-style',
+          'apex-security',
+          'csharp-best-practices',
+          'csharp-code-style',
+          'csharp-security',
+          'csharp-inclusive',
+          'docker-best-practices',
+          'github-actions',
+          'go-best-practices',
+          'go-inclusive',
+          'go-security',
+          'java-best-practices',
+          'java-code-style',
+          'java-security',
+          'javascript-best-practices',
+          'javascript-browser-security',
+          'javascript-code-style',
+          'javascript-inclusive',
+          'javascript-common-security',
+          'javascript-express',
+          'javascript-node-security',
+          'jsx-react',
+          'kotlin-security',
+          'kotlin-inclusive',
+          'kotlin-code-style',
+          'kotlin-best-practices',
+          'php-best-practices',
+          'php-security',
+          'python-best-practices',
+          'python-code-style',
+          'python-django',
+          'python-flask',
+          'python-inclusive',
+          'python-pandas',
+          'python-security',
+          'ruby-code-style',
+          'ruby-security',
+          'ruby-best-practices',
+          'rails-best-practices',
+          'swift-code-style',
+          'swift-security',
+          'swift-inclusive',
+          'tsx-react',
+          'typescript-best-practices',
+          'typescript-browser-security',
+          'typescript-code-style',
+          'typescript-common-security',
+          'typescript-express',
+          'typescript-inclusive',
+          'typescript-node-security',
+        ],
+      },
+    });
   }
 }
