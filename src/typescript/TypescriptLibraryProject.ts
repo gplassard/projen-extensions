8640179e803feceaaa5b31f41119c1de08f20568
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-08-03 20:28:13 +0200

    chore(debug): try to debug

--- src/typescript/TypescriptLibraryProject.ts
+++ src/typescript/TypescriptLibraryProject.ts
@@ -1,5 +1,6 @@
 import { TypescriptApplicationProject, TypescriptApplicationProjectOptions } from './TypescriptApplicationProject';
 import { nodeVersion } from '../github';
+import { JsonPatch } from 'projen';
 
 export type TypescriptLibraryProjectOptions = TypescriptApplicationProjectOptions
 & Required<Pick<TypescriptApplicationProjectOptions, 'packageName'>>;
@@ -16,5 +17,13 @@ export class TypescriptLibraryProject extends TypescriptApplicationProject {
     };
     super(typescriptProjectOptions);
     this.tryFindObjectFile('.github/workflows/release.yml')?.addOverride('jobs.release_npm.steps.0.with.node-version', nodeVersion(options));
+
+    // Insert a debug step into the generated release workflow to list dist/js before publib runs.
+    this.tryFindObjectFile('.github/workflows/release.yml')?.patch(
+      JsonPatch.add('/jobs/release_npm/steps/3', {
+        name: 'List dist/js contents (debug)',
+        run: `echo "Listing dist/js"\nls -la dist/js || true\necho "Listing tarball globs"\nls -la dist/js/*.tgz || true`,
+      }),
+    );
   }
 }
